e0c713403d27d7f1588740b8d8e42b00068637ba
Maryyam Tanveer <tanvirmeryem2@gmail.com>
2026-07-22 00:57:25 +0500

    Add problem description and solution outline
    Added problem statement and solution outline for duplicate integer check.

--- Data Structures & Algorithms/duplicate-integer/submission-1.py
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -1,3 +1,29 @@
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+this was the problem
+
+Solution:
+create a hashmap
+go through each element of list
+if element alr in hashmap return True
+else add that element into hashmap 
+after loop ends return False
+
+        
+
+
+
+
+
+
+
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         hashset = set()
